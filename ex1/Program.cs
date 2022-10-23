6245a38ad3e646a471c081a0cd3b131d082e6f82
Ramil <ramilkurbanov666@gmail.com>
2022-10-23 13:47:11 +0300

    сделал дз

---
+++ ex1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задание 1 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+int num = new Random().Next(100,1000);
+
+int second = num / 100;
+
+int third = num / 100;
+int result = second % 10;
+Console.Write($" {num} ===> {result}");
